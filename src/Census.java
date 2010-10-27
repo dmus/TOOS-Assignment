f58a4792a10e4eb71424624b133c4653af6d2170
Derk <Derk@Derk-PC>
2010-10-27 18:51:03 +0200

    comment

--- src/Census.java
+++ src/Census.java
@@ -3,6 +3,14 @@ import java.util.Set;
 
 public class Census {	
 	
+	/**
+	 * Return true if and only if all voters in set
+	 * vote true. Each voter must be called exactly
+	 * one time.
+	 * 
+	 * @param voters
+	 * @return
+	 */
 	public boolean census(Set<Voter> voters) {
 		boolean census = true;
 		for (Voter voter : voters) {
